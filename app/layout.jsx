e79dd71cb1211e978f143bc1316a6fe5d30451a4
Firzus <lprieu@gaming.tech>
2023-08-20 22:29:22 +0200

    init project content

---
+++ app/layout.jsx
@@ -0,0 +1,24 @@
+import '@styles/globals.css'
+import { Poppins } from "next/font/google"
+
+const poppins = Poppins({
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  subsets: ['latin'],
+})
+
+export const metadata = {
+    title: 'Wakfu Guide',
+    description: 'Default description',
+}
+
+const RootLayout = ({children}) => {
+  return (
+    <html lang='fr'>
+      <body className={poppins.className}>
+        {children}
+      </body>
+    </html>
+  )
+}
+
+export default RootLayout
