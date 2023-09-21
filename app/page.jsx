2f2e3f98e079d7db9eafa46f7922e9c7b5f647f0
Firzus <lprieu@gaming.tech>
2023-09-21 12:22:12 +0200

    add content mobile

--- app/page.jsx
+++ app/page.jsx
@@ -1,20 +1,45 @@
+import Image from "next/image"
+import Link from "next/link"
+
+// Components
+import Button from "@components/Button"
+
+// Data
+import Illustration from "@public/images/illustration.png"
+
 const Home = () => {
+
+  const linkDiscoverdWakfu = "https://youtu.be/MOnP3z6TsPw?si=6GAJ1RvrxFpw4rtX";
+
   return (
-    <main className="container pt-8 grow flex flex-col justify-between">
-      <div className="">
-        <h1>
+    <main className="container columns-4 pt-8 grow flex flex-col justify-between gap-4">
+      <div className="flex flex-col gap-4">
+        <header className="flex flex-col gap-2 w-">
+          <h1 className="title">
+            Wakfu Guide
+          </h1>
 
-        </h1>
-        <p>
+          <p className="small text-white-300">
+            La référence en tutoriels, astuces et guides sur Wakfu et sa commauté.
+          </p>
+        </header>
 
-        </p>
-        <button>
-
-        </button>
+        <Link href={linkDiscoverdWakfu} className="w-fit">
+          <Button
+            label="découvrir"
+            color="gradient"
+            size="sm"
+          />
+        </Link>
       </div>
-      <div>
 
-      </div>
+      <Image
+        src={Illustration}
+        alt="Illustration"
+        width={273}
+        height={324}
+        className="ml-auto"
+      />
     </main>
   )
 }
