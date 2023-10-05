eca2b3799c0876b353fd95d1889e42227ee075d6
lprieu <lprieu@gaming.tech>
2023-10-05 11:04:06 +0200

    fix logo size test

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -27,13 +27,11 @@ const Nav = () => {
 
     return (
         <main className='flex justify-between items-center bg-black-900'>
-            <Link href="/" className='flex w-12 h-12'>
+            <Link href="/">
                 <Image
+                    className='mx-2 xl:mx-6 w-8 xl:w-12 h-12 xl:h-20'
                     src={Logo}
                     alt='Wakfu Guide Logo'
-                    width={32}
-                    height={48}
-                    className='mx-auto'
                 />
             </Link>
 
@@ -42,11 +40,11 @@ const Nav = () => {
                 className='flex w-12 h-12'
             >
                 <Image
+                    className='mx-auto'
                     src={IconDropdown}
                     alt='Open Side Bar'
                     width={32}
                     height={32}
-                    className='mx-auto'
                 />
             </div>
 
