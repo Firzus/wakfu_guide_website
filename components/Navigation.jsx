84d097b2f55b468beca5ada080bbacd50c975050
Firzus <lprieu@gaming.tech>
2023-10-06 13:01:51 +0200

    Update Navigation.jsx

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -120,7 +120,6 @@ const Navigation = () => {
             </ul>
 
             {isNavOpen && <SideBar navbarRef={navbarRef} handleNavLinkClick={() => { setNavOpen(false) }} />}
-
         </main>
     )
 }
