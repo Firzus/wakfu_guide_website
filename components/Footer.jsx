bd0e9c2258b341e600deaa3fa94a464e2879b6d0
Firzus <lprieu@gaming.tech>
2023-10-10 14:05:49 +0200

    fix footer responsive

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <footer className="text-center text-xs text-white-300 font-light">
+        <footer className="text-center text-xs text-white-300 font-light lg:py-4">
             <p>
                 Certaines images sont la propriété d’Ankama Studio.{' '}
                 <span className="hidden md:inline">©2023 WakfuGuide.</span>
