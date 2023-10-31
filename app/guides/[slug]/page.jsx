7760bb5b5560fec95dfcda6383288ef4d9ff1256
lprieu <lprieu@gaming.tech>
2023-10-31 13:58:32 +0100

    add content

--- app/guides/[slug]/page.jsx
+++ app/guides/[slug]/page.jsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import Markdown from 'markdown-to-jsx'
+
 // Component
 import Breadcrumb from "@components/Breadcrumb";
 
@@ -10,25 +13,36 @@ export async function generateStaticParams() {
 }
 
 async function getGuide(slug) {
-    const res = await fetch(`https://wakfu-guide-api-cckfj.ondigitalocean.app/api/guides?filters[slug]=${slug}`);
+    const res = await fetch(`https://wakfu-guide-api-cckfj.ondigitalocean.app/api/guides?filters[slug]=${slug}&populate=*`, { cache: 'no-store' });
     const data = await res.json();
     return data.data[0];
 }
 
 export default async function Page({ params }) {
     const guide = await getGuide(params.slug);
+    const urlAPI = "https://wakfu-guide-api-cckfj.ondigitalocean.app"
 
     return (
         <section className="w-full grow flex flex-col gap-4">
             <Breadcrumb />
 
-            <h1 className="text-white-100">
+            <h1 className="text-white-100 text-xl font-semibold">
                 {guide.attributes.titre}
             </h1>
 
             <p className="text-white-300">
                 {guide.attributes.description}
             </p>
+
+            <Image
+                className="w-full rounded"
+                src={urlAPI + guide.attributes.illustration.data.attributes.url}
+                alt={guide.attributes.illustration.data.attributes.alternativeText == null && guide.attributes.illustration.data.attributes.name}
+                width={guide.attributes.illustration.data.attributes.width}
+                height={guide.attributes.illustration.data.attributes.height}
+            />
+
+            <Markdown className='contenu-blog'>{guide.attributes.contenu}</Markdown>
 
         </section>
     );
